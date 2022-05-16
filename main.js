8bd150b03f1210738b4ac3a1a8e3d4245117dc49
Jakub Popy <popek1814@gmail.com>
2022-05-16 18:56:30 +0200

    zmiany

--- main.js
+++ main.js
@@ -1,48 +1,47 @@
 let ipTab = [
-    [58, 50, 42, 34, 26, 18, 10, 2],
-    [60, 52, 44, 36, 28, 20, 12, 4],
-    [62, 54, 46, 38, 30, 22, 14, 6],
-    [64, 56, 48, 40, 32, 24, 16, 8],
-    [57, 49, 41, 33, 25, 17, 9, 1],
-    [59, 51, 43, 35, 27, 19, 11, 3],
-    [61, 53, 45, 37, 29, 21, 13, 5],
-    [63, 55, 47, 39, 31, 23, 15, 7]
-]
+    58, 50, 42, 34, 26, 18, 10, 2,
+    60, 52, 44, 36, 28, 20, 12, 4,
+    62, 54, 46, 38, 30, 22, 14, 6,
+    64, 56, 48, 40, 32, 24, 16, 8,
+    57, 49, 41, 33, 25, 17, 9, 1,
+    59, 51, 43, 35, 27, 19, 11, 3,
+    61, 53, 45, 37, 29, 21, 13, 5,
+    63, 55, 47, 39, 31, 23, 15, 7 ];
 
 let pc1Tab = [
-    [57, 49, 41, 33, 25, 17, 9],
-    [1, 58, 50, 42, 34, 26, 18],
-    [10, 2, 59, 51, 43, 35, 27],
-    [19, 11, 3, 60, 52, 44, 36],
-    [63, 55, 47, 39, 31, 23, 15],
-    [7, 62, 54, 46, 38, 30, 22],
-    [14, 6, 61, 53, 45, 37, 29],
-    [21, 13, 5, 28, 20, 12, 4]
-]
+    57, 49, 41, 33, 25, 17, 9,
+    1, 58, 50, 42, 34, 26, 18,
+    10, 2, 59, 51, 43, 35, 27,
+    19, 11, 3, 60, 52, 44, 36,
+    63, 55, 47, 39, 31, 23, 15,
+    7, 62, 54, 46, 38, 30, 22,
+    14, 6, 61, 53, 45, 37, 29,
+    21, 13, 5, 28, 20, 12, 4
+];
 
 let pc2Tab = [
-    [14, 17, 11, 24, 1, 5],
-    [3, 28, 15, 6, 21, 10],
-    [23, 19, 12, 4, 26, 8],
-    [16, 7, 27, 20, 13, 2],
-    [41, 52, 31, 37, 47, 55],
-    [30, 40, 51, 45, 33, 48],
-    [44, 49, 39, 56, 34, 53],
-    [46, 42, 50, 36, 29, 32]
-]
+    14, 17, 11, 24, 1, 5,
+    3, 28, 15, 6, 21, 10,
+    23, 19, 12, 4, 26, 8,
+    16, 7, 27, 20, 13, 2,
+    41, 52, 31, 37, 47, 55,
+    30, 40, 51, 45, 33, 48,
+    44, 49, 39, 56, 34, 53,
+    46, 42, 50, 36, 29, 32 
+];
 
 let shiftTab = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
 
 let eTab = [
-    [32, 1, 2, 3, 4, 5],
-    [4, 5, 6, 7, 8, 9],
-    [8, 9, 10, 11, 12, 13],
-    [12, 13, 14, 15, 16, 17],
-    [16, 17, 18, 19, 20, 21],
-    [20, 21, 22, 23, 24, 25],
-    [24, 25, 26, 27, 28, 29],
-    [28, 29, 30, 31, 32, 1]
-]
+    32, 1, 2, 3, 4, 5,
+    4, 5, 6, 7, 8, 9,
+    8, 9, 10, 11, 12, 13,
+    12, 13, 14, 15, 16, 17,
+    16, 17, 18, 19, 20, 21,
+    20, 21, 22, 23, 24, 25,
+    24, 25, 26, 27, 28, 29,
+    28, 29, 30, 31, 32, 1 
+];
 
 let s1Tab = [
     [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
@@ -101,27 +100,52 @@ let s8Tab = [
 ]
 
 let pTab = [
-    [16, 7, 20, 21],
-    [29, 12, 28, 17],
-    [1, 15, 23, 26],
-    [5, 18, 31, 10],
-    [2, 8, 24, 14],
-    [32, 27, 3, 9],
-    [19, 13, 30, 6],
-    [22, 11, 4, 25]
+    16, 7, 20, 21,
+    29, 12, 28, 17,
+    1, 15, 23, 26,
+    5, 18, 31, 10,
+    2, 8, 24, 14,
+    32, 27, 3, 9,
+    19, 13, 30, 6,
+    22, 11, 4, 25
 ]
 
 let ipMinusTab = [
-    [40, 8, 48, 16, 56, 24, 64, 32],
-    [39, 7, 47, 15, 55, 23, 63, 31],
-    [38, 6, 46, 14, 54, 22, 62, 30],
-    [37, 5, 45, 13, 53, 21, 61, 29],
-    [36, 4, 44, 12, 52, 20, 60, 28],
-    [35, 3, 43, 11, 51, 19, 59, 27],
-    [34, 2, 42, 10, 50, 18, 58, 26],
-    [33, 1, 41, 9, 49, 17, 57, 25]
-]
+    40, 8, 48, 16, 56, 24, 64, 32,
+    39, 7, 47, 15, 55, 23, 63, 31,
+    38, 6, 46, 14, 54, 22, 62, 30,
+    37, 5, 45, 13, 53, 21, 61, 29,
+    36, 4, 44, 12, 52, 20, 60, 28,
+    35, 3, 43, 11, 51, 19, 59, 27,
+    34, 2, 42, 10, 50, 18, 58, 26,
+    33, 1, 41, 9, 49, 17, 57, 25
+];
 
+
+//zamiana na system dziesietny 
+function BytesToBCD(code, length) {
+	let resultBCD = 0;
+	for (let i = length; i > 0; i--) {
+		if (code[i - 1] == "1") {
+			resultBCD += Math.pow(2, length - i);
+		}
+	}
+	return resultBCD;
+}
+
+//funkcja pomagająca zamienić char w stringu
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+}
+
+//zamiana tekstu na binarna reprezentacje z dopelnieniem do 8 znakow
+function text2Binary(string) {
+    return string.split('').map(function (char) {
+        return char.charCodeAt(0).toString(2).padStart(8, '0');
+    }).join('');
+}
+
+//funkcja permutujaca wg. tabeli danej w argumencie
 function Permutation(text, arr, n){
     let result = ""
     for(i = 0; i < n; i++){
@@ -130,3 +154,95 @@ function Permutation(text, arr, n){
     return result
 }
 
+//funkcja przechodzaca po tabeli i przesuwajaca klucz wg. odpowiednich wartosci z tabeli shift
+function leftShift(key, shift){
+    let n = key.length
+    let keyShifted = key;
+
+    for(i = 0, j = 0; i < n; i++){
+        if(i + shift < n) {
+            keyShifted = keyShifted.replaceAt(i, key.charAt(i + shift))
+        }
+        else{
+            keyShifted = keyShifted.replaceAt(i, key.charAt(j))
+            j++
+        }
+    }
+    return keyShifted
+}
+
+function XOR(arr1, arr2){
+    let n = arr1.length
+    let result = "";
+
+    for(i = 0; i < n; i++){
+        result += arr1[i] ^ arr2[i]
+    }
+    
+    return result;
+}
+
+function bitSequenceTo4(sequence, arr){
+    let rowID = sequence[0] + sequence[5]
+    let colID = sequence.substring(1,5)
+
+    let rowBCD = BytesToBCD(rowID, rowID.length)
+    let colBCD = BytesToBCD(colID, colID.length)
+    let result = arr[rowBCD][colBCD]
+    let result4digits = result.toString(2).padStart(4, '0');
+
+    console.log(result4digits)
+    return result4digits
+}
+
+function DES(text, key){
+    //console.log(text)
+    let result = text2Binary(text);
+    result = Permutation(result, ipTab, 64)
+
+    //PODZIAŁ NA BLOKI LEWY I PRAWY
+
+    let blockL = result.substring(0,32)
+    let blockR = result.substring(32,64)
+
+    //PREMUTACJA KLUCZA I SKRÓCENIE GO O 8 BITÓW
+
+    let keyBit = text2Binary(key);
+    keyBit = Permutation(keyBit, pc1Tab, 56)
+
+    //PODZIAŁ KLUCZA NA DWA BLOKI
+
+    let blockC = keyBit.substring(0,28)
+    let blockD = keyBit.substring(28,56)
+
+    //ITERACJE
+    for(i = 0; i < 1; i++){
+        blockC = leftShift(blockC, shiftTab[i])
+        blockD = leftShift(blockD, shiftTab[i])
+        let keyK = blockC + blockD
+        let keyK_permuted = Permutation(keyK, pc2Tab, 48)
+        let blockR48 = Permutation(blockR, eTab, 48)
+        let xorRes = XOR(keyK_permuted, blockR48);
+
+        let bit6seq1 = xorRes.substring(0, 6);
+        let bit6seq2 = xorRes.substring(6, 12);
+        let bit6seq3 = xorRes.substring(12, 18);
+        let bit6seq4 = xorRes.substring(18, 24);
+        let bit6seq5 = xorRes.substring(24, 30);
+        let bit6seq6 = xorRes.substring(30, 36);
+        let bit6seq7 = xorRes.substring(36, 42);
+        let bit6seq8 = xorRes.substring(42, 48);
+
+        let bit4seq1 = bitSequenceTo4(bit6seq1, s1Tab)
+        let bit4seq2 = bitSequenceTo4(bit6seq2, s2Tab)
+        let bit4seq3 = bitSequenceTo4(bit6seq3, s3Tab)
+        let bit4seq4 = bitSequenceTo4(bit6seq4, s4Tab)
+        let bit4seq5 = bitSequenceTo4(bit6seq5, s5Tab)
+        let bit4seq6 = bitSequenceTo4(bit6seq6, s6Tab)
+        let bit4seq7 = bitSequenceTo4(bit6seq7, s7Tab)
+        let bit4seq8 = bitSequenceTo4(bit6seq8, s8Tab)
+
+    }
+}
+
+DES("aleksander", "aleksander")
